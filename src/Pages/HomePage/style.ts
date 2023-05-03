f3c7d685c38d7870fedbe5ba99b471587a500c80
ThalissonMaximino <thalissonmaximino@gmail.com>
2023-05-03 15:51:13 -0300

    add:Homepage

---
+++ src/Pages/HomePage/style.ts
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+
+export const StyledMain = styled.main`
+background-color: #4F4F4F;
+display: flex;
+flex-direction: row;
+padding: 20px;
+
+`
+
+export const StyledDivButtons = styled.div`
+display: flex;
+flex-direction: column;
+padding:8px;
+align-items: center;
+gap:16px;
+`
+
+export const StyledLoginButton = styled.button`
+background-color: #AE8B63;
+height: 40px;
+width: 30%;
+border-radius:8px;
+color: white;
+font-size: 20px;
+padding:5px;
+`
+
+export const StyledRegisterButton = styled.button`
+background-color: #905F2E;
+height: 40px;
+width: 30%;
+border-radius:8px;
+color: white;
+font-size: 20px;
+padding:5px;
+`
+
+export const StyledDivSmallImg = styled.div`
+display: flex;
+flex-direction: row;
+flex-wrap: wrap;
+padding:20px;
+gap:16px;
+align-items: center;
+`
---
+++ src/components/Form/Header/index.tsx
@@ -0,0 +1,14 @@
+import { StyledHeader } from "./style"
+
+
+export const Header = () => {
+
+
+    return(
+        <>
+        <StyledHeader>
+            <h1>Kenzie Museu</h1>
+        </StyledHeader>
+        </>
+    )
+}

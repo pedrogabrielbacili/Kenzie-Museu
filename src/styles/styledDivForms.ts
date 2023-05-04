efd0a83416f9846cade8bf3a586e61ce0484ca2f
jonatasneres <jonatas102009@hotmail.com>
2023-05-04 10:31:32 -0300

    feat: estilização página login / cadastro

---
+++ src/styles/styledDivForms.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import img from "../assets/imgform.svg";
+
+export const StyledDivForm = styled.div`
+  background-image: url(${img});
+  background-repeat: no-repeat;
+  background-size: cover;
+
+  height: 100vh;
+  width: 100%;
+
+  text-align: center;
+  position: absolute;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+
+  
+
+`;

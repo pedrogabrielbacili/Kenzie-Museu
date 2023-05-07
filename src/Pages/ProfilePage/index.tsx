cc0919b83fd0729845879cf411da44af555c00cf
Jandesson Moraes <jandessonmoraes@gmail.com>
2023-05-06 21:50:12 -0300

    feat: ajuste da profile page

--- src/Pages/ProfilePage/index.tsx
+++ src/Pages/ProfilePage/index.tsx
@@ -1,34 +1,38 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 import retangle from "../../assets/rectangle.svg";
-import homeButton from "../../assets/homeButton.png";
+import home from "../../assets/homee.svg";
 import { StyledHeader, StyledMain } from "./style";
 import { useNavigate } from "react-router-dom";
 import { UserContext } from "../../providers/UserContext";
+
 const ProfilePage = () => {
   const navigate = useNavigate();
 
   const homeNavigate = () => {
     navigate("/dashboard");
   };
+
   const { user } = useContext(UserContext);
 
   return (
     <>
       <StyledHeader>
-        <h1 className="logo">Kenzie Museu</h1>
-        <button>
-          <img src={homeButton} alt="Ir para inicio" onClick={homeNavigate} />
-        </button>
+        <nav>
+          <h1 className="logo">Kenzie Museu</h1>
+          <button>
+            <img src={ home } alt="Ir para inicio" onClick={homeNavigate} />
+          </button>
+        </nav>
       </StyledHeader>
       {user ? (
         <StyledMain>
           <img className="img-bg" src={retangle} alt="" />
           <div className="profile-container">
-            <p>Bem vindo de volta, {user?.name}</p>
+            <p>Bem vindo(a), {user?.name}</p>
             <p>{user?.email}</p>
           </div>
           <div className="favorite-container">
-            <p>Você ainda não possuí nenhuma obra favorita</p>
+            <p>Você ainda não possuí nenhuma obra favorita.</p>
             <button>Adicionar Favoritos</button>
           </div>
         </StyledMain>
--- src/Pages/ProfilePage/style.tsx
+++ src/Pages/ProfilePage/style.tsx
@@ -5,6 +5,25 @@ export const StyledHeader = styled.header`
   display: flex;
   justify-content: space-between;
   padding: 0.5rem 1rem;
+  margin: 0 auto;
+  justify-content: center;
+
+  nav {
+    display: flex;
+    justify-content: space-between;
+    width: 1150px;
+    button {
+      background-color: #d3b98c;
+      img {
+        color: #30343a;
+        background-color: #d3b98c;
+
+      }
+    }
+    h1 {
+      color: #30343a;
+    }
+  }
 `;
 
 export const StyledMain = styled.main`
@@ -12,12 +31,14 @@ export const StyledMain = styled.main`
   font-weight: 600;
   position: relative;
   padding: 2rem 4rem;
+  width: 1200px;
+  margin: 0 auto;
   .img-bg {
     position: absolute;
-    top: 19vh;
+    top: 22vh;
     width: 100%;
-    height: 90vh;
-    left: -20px;
+    height: 450px;
+    left: -40px;
     z-index: -1;
   }
   .profile-container {
@@ -38,6 +59,13 @@ export const StyledMain = styled.main`
       background-color: rgba(107, 84, 58, 1);
       color: #ffff;
       border-radius: 10px;
+      transition: 0.5s ease;
+      font-weight: 600;
+      :hover {
+        background-color: #99764e;
+        box-shadow: 1px 1px 2px 1px rgba(0, 0, 0, 0.5);
+
+      }
     }
   }
   @media (min-width: 600px) {

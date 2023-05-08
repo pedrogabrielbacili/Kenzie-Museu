bf19bc7ba0c6c8ed0c89dbcf474e42138fd8ada3
pedrogabrielbacili <pedrobacili1@gmail.com>
2023-05-08 09:23:14 -0300

    bugs

--- src/Pages/InfoArtsPage/index.tsx
+++ src/Pages/InfoArtsPage/index.tsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { useParams } from "react-router-dom";
 import { api } from "../../service/api";
-import {  ICard } from "../../providers/CardContext";
+import { CardContext, ICard } from "../../providers/CardContext";
+
 import { StyledArtDiv, StyledMain, StyledSpanBtnDiv } from "./style";
 import { HeaderInfoArts } from "../../components/HeaderInfoArts";
 
@@ -9,7 +10,7 @@ import { HeaderInfoArts } from "../../components/HeaderInfoArts";
 export const InfoArtPage = () => {
   const { id } = useParams();
 
-  const [art, setArt] = useState<ICard | null>(null);
+  const [art, setArt] = useState<ICard>({} as ICard);
 
   const loadArt = async () => {
     try {
@@ -23,30 +24,31 @@ export const InfoArtPage = () => {
     loadArt();
   }, [id]);
 
+  const { setFavorite, favorite } = useContext(CardContext);
+
+  const handleClick = () => {
+    setFavorite([...favorite, art]);
+  };
+
   return (
     <>
       <HeaderInfoArts />
       <StyledMain>
         <StyledArtDiv>
-          <section>
-            <div className="divImg">
-              {" "}
-              <img src={art?.img} />
-            </div>
-            <div className="divTitle">
-              <span>Title:</span>
-              <h2>{art?.title}</h2>
-              <span>Artist/Culture:</span>
-              <h2> {art?.artist}</h2>
-              <span>Date:</span>
-              <h2>{art?.date}</h2>
-              <StyledSpanBtnDiv>
-                <span>Dimensions:</span>
-                <h4>{art?.dimensions}</h4>
-                <button>Adicionar aos favoritos</button>
-              </StyledSpanBtnDiv>
-            </div>
-          </section>
+
+          <div>
+            <h1>{art?.title}</h1>
+            <h2>{art?.artist}</h2>
+            <h3>date:{art?.date}</h3>
+            <StyledSpanBtnDiv>
+              <span>dimensions:{art?.dimensions}</span>
+              <button onClick={handleClick}>Adicionar aos favoritos</button>
+            </StyledSpanBtnDiv>
+          </div>
+          <div>
+            {" "}
+            <img src={art?.img} />
+          </div>
         </StyledArtDiv>
       </StyledMain>
     </>
--- src/Pages/ProfilePage/index.tsx
+++ src/Pages/ProfilePage/index.tsx
@@ -4,6 +4,8 @@ import home from "../../assets/homee.svg";
 import { StyledHeader, StyledMain } from "./style";
 import { useNavigate } from "react-router-dom";
 import { UserContext } from "../../providers/UserContext";
+import { CardContext } from "../../providers/CardContext";
+import { CardArts } from "../../components/Form/ListArts/CardArts";
 
 const ProfilePage = () => {
   const navigate = useNavigate();
@@ -14,6 +16,8 @@ const ProfilePage = () => {
 
   const { user } = useContext(UserContext);
 
+  const { favorite } = useContext(CardContext);
+
   return (
     <>
       <StyledHeader>
@@ -23,9 +27,9 @@ const ProfilePage = () => {
             <img src={home} alt="Ir para inicio" onClick={homeNavigate} />
           </button>
         </nav>
-      </StyledHeader>
-      
+      </StyledHeader>   
       {user ? (
+     
         <StyledMain>
           <div className="profile-container">
             <p>Bem vindo(a), {user?.name}</p>
@@ -41,8 +45,16 @@ const ProfilePage = () => {
           <img className="img-bg" src={retangle} alt="" />
           <div className="profile-container"></div>
           <div className="favorite-container">
-            <p>Você ainda não possuí nenhuma obra favorita</p>
-            <button onClick={homeNavigate}>Adicionar Favoritos</button>
+            <p>Minhas obras favoritas</p>
+            {favorite.map((art) => (
+              <CardArts
+                key={art.id}
+                id={art.id}
+                img={art.img}
+                title={art.title}
+                artist={art.artist}
+              />
+            ))}
           </div>
         </StyledMain>
       )}
